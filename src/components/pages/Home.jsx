fde55ddb418937d5d20cf45df4d946ecea6c463c
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-12-19 11:31:25 +0500

    Done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Home from './src/components/pages/Home'
 
-const App = () => {
+const Home = () => {
   return (
     <View>
-      <Home/>
+      <Text>Home</Text>
     </View>
   )
 }
 
-export default App
+export default Home
