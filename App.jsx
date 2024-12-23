385c93ca444c16c112bf087afce6a2bf1f895f71
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-12-23 22:58:58 +0500

    Done

--- App.jsx
+++ App.jsx
@@ -2,12 +2,18 @@ import { StatusBar, View } from 'react-native'
 import React from 'react'
 import Home from './src/components/pages/Home'
 
+
+const Stack = createNativeStackNavigator();
+
+
 const App = () => {
   return (
     <View style={{flex: 1}}> 
       <StatusBar backgroundColor='red'/>
-      <Home />
-    </View>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </View>
   )
 }
 
