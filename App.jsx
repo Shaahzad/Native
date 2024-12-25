05e6566612429c64ebb3414f14a1e71bf74d1f52
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-12-25 19:13:27 +0500

    Done

--- App.jsx
+++ App.jsx
@@ -3,14 +3,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/pages/Home';
 import Login from './src/pages/Login';
-import { StatusBar, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
     <StatusBar backgroundColor={'red'}/>
      <NavigationContainer>
       <Stack.Navigator>
@@ -23,5 +23,12 @@ const App = () => {
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+      flex: 1
+  }
+})
 
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,100 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const Home = ({navigation}) => {
 console.log(navigation)
 
   return (
-    <View>
-      <Text>Home</Text>
-      <Button title='Go To'   
-      onPress={() => navigation.navigate('Login')} 
-      />
+    <View style={styles.container}>
+      <View style={styles.heading}>
+        <View style={styles.headingtext}>
+        <Text style={styles.signup}>Sign Up</Text>
+        <Text style={styles.signupchild}>Create Your Account</Text>
+        </View>
+      </View>
+      <View style={styles.forminput}>
+        <TextInput placeholder='User Name' style={styles.input}/>
+        <TextInput placeholder='Email' style={styles.input}/>
+        <TextInput placeholder='Password' style={styles.input}/>
+        <TextInput placeholder='Confirm Password' style={styles.input}/>
+        <View style={styles.btncontainer}>
+        <TouchableOpacity style={styles.signupbtn}>
+            <Text style={styles.btntext}>Sign Up</Text>
+        </TouchableOpacity>
+        <Text style={styles.or}>Or</Text>
+        <TouchableOpacity  style={styles.loginbtn}>
+            <Text style={styles.btntextlogin}>login with Google</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
     </View>
   ) 
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+    },
+    heading:{
+        flex: 1,
+    },
+    headingtext:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+    signup:{
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    signupchild:{
+        fontSize: 12
+},
+    forminput:{
+        flex: 2,
+        padding: 20,
+        gap: 12,
+    },
+    input:{
+        backgroundColor: '#fae6e0',
+        padding: 10,
+        borderRadius: 10,
+        height: 60,
+    },
+    btncontainer:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10
+    },
+    or:{
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    loginbtn:{
+        borderWidth: 2,
+        borderColor: '#EF4F1F', 
+        padding: 12,
+        borderRadius: 30,
+        width: '100%'
+    },
+    signupbtn:{
+        backgroundColor: '#EF4F1F',
+        padding: 12,
+        borderRadius: 30,
+        width: 250,
+        alignSelf: 'center',
+    },
+    btntext:{
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    btntextlogin:{
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 15,
+    }
+})
